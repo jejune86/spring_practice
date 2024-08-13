b526796a5ee1dc5669e908b5865da04282bcd130
jejune86 <jejune86@gmail.com>
2024-08-13 16:56:56 +0900

    REST Endpoint - 2

--- src/main/java/com/example/starthere7/ProjectConfig.java
+++ src/main/java/com/example/starthere7/ProjectConfig.java
@@ -1,9 +1,20 @@
 package com.example.starthere7;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.example.starthere7.proxy")
+//FeignClient
+//@EnableFeignClients(basePackages = "com.example.starthere7.proxy")
 public class ProjectConfig {
+
+    //WebClient
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
 }
+
+
--- src/main/java/com/example/starthere7/controller/PaymentsController.java
+++ src/main/java/com/example/starthere7/controller/PaymentsController.java
@@ -3,12 +3,14 @@ package com.example.starthere7.controller;
 import com.example.starthere7.model.Payment;
 import com.example.starthere7.model.PaymentDetails;
 import com.example.starthere7.proxy.PaymentProxy;
+import com.example.starthere7.proxy.PaymentsProxy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -16,16 +18,28 @@ import java.util.logging.Logger;
 @RestController
 public class PaymentsController {
 
-    private final PaymentProxy paymentProxy;
+//    private final PaymentProxy paymentProxy;
+//
+//    public PaymentsController(PaymentProxy paymentProxy) {
+//        this.paymentProxy = paymentProxy;
+//    }
+//
+//    @PostMapping("/payment")
+//    public Payment CreatePayment(@RequestBody Payment payment) {
+//        String requestId = UUID.randomUUID().toString();
+//        return paymentProxy.createPayment(requestId, payment);
+//    }
 
-    public PaymentsController(PaymentProxy paymentProxy) {
-        this.paymentProxy = paymentProxy;
+    private final PaymentsProxy paymentsProxy;
+
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
     }
 
     @PostMapping("/payment")
-    public Payment CreatePayment(@RequestBody Payment payment) {
+    public Mono<Payment> createPayment(@RequestBody Payment payment) {
         String requestId = UUID.randomUUID().toString();
-        return paymentProxy.createPayment(requestId, payment);
+        return paymentsProxy.createPayment(requestId, payment);
     }
 
 }
